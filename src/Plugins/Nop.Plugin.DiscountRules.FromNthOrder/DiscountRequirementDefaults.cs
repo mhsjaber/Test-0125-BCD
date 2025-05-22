069489a56a98d0961fc07389e3bf0653a299d01c
mhsjaber@gmail.com <mhsjaber@gmail.com>
2025-05-22 23:15:34 +0600

    Discount from Nth order implemented

---
+++ src/Plugins/Nop.Plugin.DiscountRules.FromNthOrder/DiscountRequirementDefaults.cs
@@ -0,0 +1,22 @@
+﻿namespace Nop.Plugin.DiscountRules.FromNthOrder;
+
+/// <summary>
+/// Represents defaults for the discount requirement rule
+/// </summary>
+public static class DiscountRequirementDefaults
+{
+    /// <summary>
+    /// The system name of the discount requirement rule
+    /// </summary>
+    public static string SystemName => "DiscountRequirement.FromNthOrder";
+
+    /// <summary>
+    /// The key of the settings to save restricted customer roles
+    /// </summary>
+    public static string SettingsKey => "DiscountRequirement.FromNthOrder-{0}";
+
+    /// <summary>
+    /// The HTML field prefix for discount requirements
+    /// </summary>
+    public static string HtmlFieldPrefix => "DiscountRulesFromNthOrder{0}";
+}
---
+++ src/Plugins/Nop.Plugin.DiscountRules.FromNthOrder/Models/RequirementModel.cs
@@ -0,0 +1,13 @@
+﻿using Nop.Web.Framework.Mvc.ModelBinding;
+
+namespace Nop.Plugin.DiscountRules.FromNthOrder.Models;
+
+public class RequirementModel
+{
+    [NopResourceDisplayName("Plugins.DiscountRules.FromNthOrder.Fields.NthOrder")]
+    public int NthOrder { get; set; }
+
+    public int DiscountId { get; set; }
+
+    public int RequirementId { get; set; }
+}
